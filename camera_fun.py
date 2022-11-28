be2c04807b00af137bdbdf12162bcc96676c35fb
mriojik <kuznetsov.ivan.n@gmail.com>
2022-11-28 15:40:29 +0300

    camera fun final

--- camera_fun.py
+++ camera_fun.py
@@ -1,10 +1,9 @@
 import numpy as np
 
+
 def camera_move(x, y, pic):
     camera_view = np.zeros((405, 720, 4))
     for i in range(405):
         for j in range(720):
-            camera_view[i][j]=pic[x-360+i][y-202+j]
+            camera_view[i][j] = pic[x - 360 + i][y - 202 + j]
     return camera_view
-
-
