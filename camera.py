c22f4d4628c4d30d44eec8b2fd93d3748844da79
Kristina Unixovna <secret@secret.secret>
2022-12-16 14:29:42 +0300

    Reformating, added readme and requirements.txt

--- camera.py
+++ camera.py
@@ -1,28 +1,28 @@
-import numpy as np
 from config import *
 
+
 def camera_move(x, y):
-    return (x-WIDTH//2, y - HEIGHT//2, x + WIDTH//2, y + HEIGHT//2)
+    return x - WIDTH // 2, y - HEIGHT // 2, x + WIDTH // 2, y + HEIGHT // 2
 
 
 def edge_handing(x, y, width, height):
     """Останавливает движение камеры при приближении к краю"""
-    if x <= WIDTH//2:
-        x_to_array = WIDTH//2
-        x_to_render =  x
-    elif  x >= width - WIDTH//2:
-        x_to_array = width - WIDTH//2
-        x_to_render =  x - width + WIDTH
+    if x <= WIDTH // 2:
+        x_to_array = WIDTH // 2
+        x_to_render = x
+    elif x >= width - WIDTH // 2:
+        x_to_array = width - WIDTH // 2
+        x_to_render = x - width + WIDTH
     else:
-        x_to_array =  x
-        x_to_render = WIDTH//2
-    if  y <= HEIGHT//2:
-        y_to_array = HEIGHT//2
-        y_to_render =  y
-    elif  y >= height - HEIGHT//2:
-        y_to_array = height - HEIGHT//2
+        x_to_array = x
+        x_to_render = WIDTH // 2
+    if y <= HEIGHT // 2:
+        y_to_array = HEIGHT // 2
+        y_to_render = y
+    elif y >= height - HEIGHT // 2:
+        y_to_array = height - HEIGHT // 2
         y_to_render = y - height + HEIGHT
     else:
-        y_to_array =y
-        y_to_render = HEIGHT//2
-    return (x_to_array, x_to_render, y_to_array, y_to_render)
+        y_to_array = y
+        y_to_render = HEIGHT // 2
+    return x_to_array, x_to_render, y_to_array, y_to_render
--- enemy_controller.py
+++ enemy_controller.py
@@ -1,48 +1,53 @@
-from config import *
-import pygame
 from math import atan2, pi, sqrt, sin, cos
-from random import randint, random
+
+import pygame
 import numpy as np
+
+from config import *
+from random import randint, random
 from animations import Animation, Animator
+
 """Анимации врагов"""
+
 
 def get_range(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
+
 def get_direction(x1, y1, x2, y2):
-    direction = np.array([x2-x1, y2 - y1])
+    direction = np.array([x2 - x1, y2 - y1])
     return direction / np.linalg.norm(direction)
+
 
 def get_animation(filenames):
     animations = []
     for filename in filenames:
-        
         animations.append(pygame.transform.scale2x(pygame.image.load(filename).convert_alpha()))
     return Animation(animations, 20)
 
 
 class Enemy:
     def __init__(self, x, y, v, vision_range, health) -> None:
-        self.type = randint(0,2)
-        self.v =  v/(self.type + 4) * 4
+        self.type = randint(0, 2)
+        self.v = v / (self.type + 4) * 4
         self.animator = Animator([get_animation(PATH[self.type])])
         self.vision_range = vision_range
         self.health = health * sqrt(self.type + 1)
         self.direction = [(random() - 1) / 200, (random() - 1) / 200]
         self.rect = pygame.Rect(x - 25, y - 25, 50, 50)
-        self.rect_to_draw = pygame.Rect(self.rect.x -25, self.rect.y - 25, 50, 50)
+        self.rect_to_draw = pygame.Rect(self.rect.x - 25, self.rect.y - 25, 50, 50)
         self.visible = False
 
     def draw(self, screen, x, y):
         """Отрисовка врага"""
         if self.visible:
-            image = pygame.transform.rotate(self.animator.get_sprite(1),  -atan2(self.direction[1], self.direction[0]) * 180 / pi)
-            self.rect = image.get_rect(center = self.rect.center)
-            self.rect_to_draw = image.get_rect(center = self.rect.center)
-            self.rect_to_draw.x = self.rect.x - x - 10 + WIDTH/2
-            self.rect_to_draw.y = self.rect.y - y - 10 + HEIGHT/2
+            image = pygame.transform.rotate(self.animator.get_sprite(1),
+                                            -atan2(self.direction[1], self.direction[0]) * 180 / pi)
+            self.rect = image.get_rect(center=self.rect.center)
+            self.rect_to_draw = image.get_rect(center=self.rect.center)
+            self.rect_to_draw.x = self.rect.x - x - 10 + WIDTH / 2
+            self.rect_to_draw.y = self.rect.y - y - 10 + HEIGHT / 2
             screen.blit(image, (self.rect_to_draw.x, self.rect_to_draw.y))
-
 
     def is_in_wall(self, walls):
         """Проверка, в стене ли враг"""
@@ -61,15 +66,17 @@ class Enemy:
 
     def search_player(self, player):
         """функция, которая ориентирует движение врагов в направлении игрока"""
-        self.direction = get_direction(self.rect.x, self.rect.y, player.rect.x + player.w/2, player.rect.y + player.h/2)
+        self.direction = get_direction(self.rect.x, self.rect.y, player.rect.x + player.w / 2,
+                                       player.rect.y + player.h / 2)
         self.animator.start_animation()
-    
+
     def move(self):
         """движение врага"""
         self.rect.x += self.direction[0] * self.v
         self.rect.y += self.direction[1] * self.v
 
-class Enemy_Controller:
+
+class EnemyController:
     def __init__(self, screen, spawn_time, enemy_power, player, power_up_time) -> None:
         self.screen = screen
         self.max_enemies = 10
@@ -83,7 +90,7 @@ class Enemy_Controller:
         self.enemy_counter = 0
         self.timer = 0
         self.player = player
-    
+
     def wall_handler(self, walls):
         """Проверка, видно ли врагов"""
         for enemy in self.enemies.values():
@@ -112,7 +119,6 @@ class Enemy_Controller:
         for i in to_delete:
             self.enemies.pop(i)
 
-    
     def is_atacked(self):
         """Функция, проверяющая, атакован ли враг, и в зависимости от результата наносящая ему урон"""
         to_delete = []
@@ -125,13 +131,14 @@ class Enemy_Controller:
                         if enemy.get_damage(orbit.damage):
                             to_delete.append(i)
                             self.player.get_exp(self.exp_for_enemy * sqrt(1 + enemy.type))
-                            exp +=  int(self.exp_for_enemy * sqrt(1 + enemy.type))
+                            exp += int(self.exp_for_enemy * sqrt(1 + enemy.type))
                             self.player.heal(orbit.damage * orbit.vampire * 2)
         to_delete = set(to_delete)
         for i in to_delete:
             self.enemies.pop(i)
         self.spawn_enemy()
         return exp
+
     def draw_enemy(self, x, y):
         """Отрирсовка врагов"""
         for i in self.enemies.values():
@@ -146,7 +153,9 @@ class Enemy_Controller:
     def spawn_enemy(self):
         """Появление врагов"""
         for i in range(self.max_enemies - len(self.enemies)):
-            r = random()* 1000 + 400
+            r = random() * 1000 + 400
             angle = random() * 2 * pi
-            self.enemies[self.enemy_counter] = Enemy(self.player.rect.x + r * cos(angle), self.player.rect.y + r * sin(angle), 4, 500, 60 * self.enemy_power / 10)
+            self.enemies[self.enemy_counter] = Enemy(self.player.rect.x + r * cos(angle),
+                                                     self.player.rect.y + r * sin(angle), 4, 500,
+                                                     60 * self.enemy_power / 10)
             self.enemy_counter += 1
